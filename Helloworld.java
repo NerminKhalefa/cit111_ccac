c1b84328b3a79748da5115c638ad376f1f5126d6
NerminKhalefa <43618286+NerminKhalefa@users.noreply.github.com>
2018-11-05 13:52:46 -0500

    Add files via upload

---
+++ Helloworld.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld;
+
+/**
+ *
+ * @author nermin.khalefa
+ */
+public class Helloworld {
+
+    /**
+    // type variable_name;
+      String personName ;
+       "Collion";
+      int age= 25;
+      //decalre int variable 
+      //flow 
+      // declare int variable 
+      final int CAR_RENTAL_AGE = 25; 
+      
+      // closes if/ else 
+      // delcare and inti
+      
+                System.out.println();
+        }
+        // TODO code application logic here
+    } // close main method 
+    **/
+} // close class 
